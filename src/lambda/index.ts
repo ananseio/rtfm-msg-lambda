255a7ebfc76f39d7fba4cbb00569e4f915923dea
Beda Tse <beda@ananse.io>
2017-08-30 18:41:58 +0800

    get data lambda

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,5 +1,5 @@
 import { SNSHeartbeatMessageHandler } from './sns-process';
-import { SNSDataQueryHandler } from './get-data';
+import { SNSDataQueryHandler } from './api/get-data';
 
 export = {
   'sns-process': SNSHeartbeatMessageHandler.handler,
